f10b4ba0f688745f034d4726cf7402bbab48b44d
Snehal Singh <53808476+Snehal-Singh174@users.noreply.github.com>
2023-04-08 22:47:03 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:animation/animation/example1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      home: const Example1(),
+    );
+  }
+}
