a4be6b01fd3e14a5b92c77e34a946c674b9a4e07
Snehal Singh <53808476+Snehal-Singh174@users.noreply.github.com>
2023-04-16 20:53:28 +0530

    working on hero animation

---
+++ lib/animation/example4.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Example4 extends StatefulWidget {
+  const Example4({Key? key}) : super(key: key);
+
+  @override
+  State<Example4> createState() => _Example4State();
+}
+
+class _Example4State extends State<Example4> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
